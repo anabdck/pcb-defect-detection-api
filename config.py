bcfd61c415ed8ab2753037d9336f8e22caa6423b
Ana C Banderchuk <ana.banderchuk@gmail.com>
2021-03-01 18:48:45 -0300

    sou um commit preguiçoso

---
+++ config.py
@@ -0,0 +1,6 @@
+import os.path
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+DEBUG = True
+
+SECRET_KEY = 'banana'
---
+++ run.py
@@ -0,0 +1,5 @@
+from app import manager
+
+
+if __name__ == "__main__":
+    manager.run()
