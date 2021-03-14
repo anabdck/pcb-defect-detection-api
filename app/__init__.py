7e37e3104408b01eb3348bb06d09ee70ca68241d
Ana C Banderchuk <ana.banderchuk@gmail.com>
2021-03-14 11:22:32 -0300

    incluido flask-script server

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
-from flask_script import Manager
+from flask_script import Manager, Server
 
 app = Flask(__name__)
 app.config.from_object('config')
 
 manager = Manager(app)
+manager.add_command("runserver", Server())
+server = Server(host="0.0.0.0", port=9000)
 
 from app.controllers import default
